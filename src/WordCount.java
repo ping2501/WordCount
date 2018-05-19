457727b81253246e7a5abb8d8ffb742594bf5595
Ojasvi Bhargava <ojasvi.bhargava96@gmail.com>
2018-05-19 16:24:31 +0530

    Initial commit

---
+++ src/WordCount.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Ojasvi on 27-01-2018.
+ */
+
+import java.io.File;
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String[]args)throws Exception{
+        File file=new File("pg30127.txt");
+        Scanner scanner=new Scanner(file);
+        int words=0;
+        while(scanner.hasNextLine()){
+            String line=scanner.nextLine();
+            words+=(line.split(" ").length);
+        }
+
+      System.out.println("The file contains: "+words+" words.");
+    }
+
+}
